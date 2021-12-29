b228c92630ad98d1ad93bd77644d3638ade47551
Michiel Dhadamus <michieldhadamus@gmail.com>
2021-12-29 17:02:17 +0100

    fixed setup

--- solution/setup.py
+++ solution/setup.py
@@ -5,13 +5,18 @@ from os.path import splitext
 
 from setuptools import find_packages, setup
 
-
 setup(
     name="academy_capstone",
     version="0.0.1",
     python_requires=">=3.7, <3.8",
-    packages=find_packages("academy_capstone"),
-    # package_dir={"": "academy_capstone"},
-    py_modules=[splitext(basename(path))[0] for path in glob("academy_capstone/*.py")],
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    # py_modules=[splitext(basename(path))[0] for path in glob("academy_capstone/*.py")],
     include_package_data=True,
+    install_requires=["awswrangler==2.9.0",
+                      "boto3==1.18.0",
+                      "botocore==1.21.0",
+                      "py-openaq==1.1.0",
+                      "pyspark==3.1.2",
+                      "pytest==6.2.4"],
 )
