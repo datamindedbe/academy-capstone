9770a7a30c84396bc0360cffcf5246e6db69ef63
Michiel Dhadamus <michieldhadamus@gmail.com>
2021-12-30 15:34:19 +0100

    Updated pythonversion

--- solution/src/academy_capstone/util/snowflake.py
+++ solution/src/academy_capstone/util/snowflake.py
@@ -1,8 +1,15 @@
-import awswrangler as wr
+import json
+
+import boto3
 
 
-def get_snowflake_creds_from_sm(secret_name: str, b3_session):
-    creds = wr.secretsmanager.get_secret_json(secret_name, b3_session)
+def get_snowflake_creds_from_sm(secret_name: str, b3_session:boto3.Session):
+    client = b3_session.client('secretsmanager')
+
+    response = client.get_secret_value(
+        SecretId=secret_name
+    )
+    creds = json.loads(response['SecretString'])
     return {
         "sfURL": f"{creds['URL']}.snowflakecomputing.com",
         "sfPassword": creds["PASSWORD"],
